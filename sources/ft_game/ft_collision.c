4983c5604c0454e0e93a2d8b5f2e9ab79ef0b27a
Tom Lasseret <tlassere@k0r4p2.42mulhouse.fr>
2024-03-30 23:23:48 +0100

    feat: pars only 4 point

--- sources/ft_game/ft_collision.c
+++ sources/ft_game/ft_collision.c
@@ -6,7 +6,7 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 17:31:21 by tlassere          #+#    #+#             */
-/*   Updated: 2024/03/30 17:32:06 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/03/30 23:23:22 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,24 +40,30 @@ bool	ft_is_hit(t_data *data, t_vec pos)
 	return (status);
 }
 
+int	ft_collision_hitbox(size_t valx1, size_t valx2, size_t valy1, size_t valy2)
+{
+	return ((ft_collision(valx1, valx2)
+			|| ft_collision(valx1 + SCALE - 1, valx2))
+		&& (ft_collision(valy1, valy2)
+			|| ft_collision(valy1 + SCALE - 1, valy2)));
+}
+
 bool	ft_is_hitbox(t_data *data, t_vec pos)
 {
 	bool	status;
-	size_t	i;
 
 	status = false;
-	i = 0;
-	while (i / data->map_size.x < data->map_size.y && status == false)
+	if ((data->map[pos.y / SCALE][pos.x / SCALE]
+		&& ft_collision_hitbox(pos.x, pos.x / SCALE, pos.y, pos.y / SCALE))
+		|| (data->map[pos.y / SCALE + 1][pos.x / SCALE]
+		&& ft_collision_hitbox(pos.x, pos.x / SCALE, pos.y, pos.y / SCALE + 1))
+		|| (data->map[pos.y / SCALE][pos.x / SCALE + 1]
+		&& ft_collision_hitbox(pos.x, pos.x / SCALE + 1, pos.y, pos.y / SCALE))
+		|| (data->map[pos.y / SCALE + 1][pos.x / SCALE + 1]
+		&& ft_collision_hitbox(pos.x, pos.x / SCALE + 1, pos.y,
+			pos.y / SCALE + 1)))
 	{
-		if (data->map[i / data->map_size.x][i % data->map_size.x] == WALL)
-		{
-			if ((ft_collision(pos.x, i % data->map_size.x)
-					|| ft_collision(pos.x + SCALE - 1, i % data->map_size.x))
-				&& (ft_collision(pos.y, i / data->map_size.x)
-					|| ft_collision(pos.y + SCALE - 1, i / data->map_size.x)))
-				status = true;
-		}
-		i++;
+		status = true;
 	}
 	return (status);
 }
