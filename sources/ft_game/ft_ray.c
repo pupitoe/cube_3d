a9d918eb85043d545bff3adda6ff7d6edab7e752
Tom Lasseret <tlassere@k1r1p3.42mulhouse.fr>
2024-04-05 14:52:20 +0200

    feat: multie ray dda end print in map

--- sources/ft_game/ft_ray.c
+++ sources/ft_game/ft_ray.c
@@ -6,7 +6,7 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 20:14:00 by tlassere          #+#    #+#             */
-/*   Updated: 2024/03/28 22:41:25 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/04/05 14:47:31 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,45 @@ void	ft_put_ray(mlx_image_t *image, float corner, t_vec size)
 			-(int)lround(pos * rets) + MAP_SIZE_OBJECT / 2, PINK);
 		pos++;
 	}
+}
+
+static void	ft_print_ray(t_data *data, float rad)
+{
+	int		pos;
+	double	retc;
+	double	rets;
+	t_ivec	c_player;
+	t_ivec	pos_pixel;
+
+	retc = cos((data->player.rotat + rad) * PI180);
+	rets = -sin((data->player.rotat + rad) * PI180);
+	c_player.x = data->player.x * MAP_SIZE_OBJECT / SCALE + MAP_SIZE_OBJECT / 2;
+	c_player.y = data->player.y * MAP_SIZE_OBJECT / SCALE + MAP_SIZE_OBJECT / 2;
+	pos = 0;
+	while (pos < 100)
+	{
+		pos_pixel.x = (int)lround(pos * retc) + c_player.x;
+		pos_pixel.y = (int)lround(pos * rets) + c_player.y;
+		if (pos_pixel.x >= 0 && pos_pixel.y >= 0
+			&& (size_t)pos_pixel.x < data->map_size.x * MAP_SIZE_OBJECT
+			&& (size_t)pos_pixel.y < data->map_size.y * MAP_SIZE_OBJECT)
+			mlx_put_pixel(data->img.map, pos_pixel.x, pos_pixel.y, PINK);
+		pos++;
+	}
+}
+
+void	ft_print_many_ray(t_data *data)
+{
+	int		ray_number;
+	float	rad;
+
+	ray_number = COUNT_RAY_MAP;
+	rad = FOV / COUNT_RAY_MAP;
+	while (ray_number)
+	{
+		ft_print_ray(data, (float)ray_number * rad);
+		ft_print_ray(data, -((float)ray_number * rad));
+		ray_number--;
+	}
+	ft_print_ray(data, 0);
 }
