94a96df4cc1b0eb8cb005953a9be546b81707c1a
Tom Lasseret <tlassere@k1r1p5.42mulhouse.fr>
2024-04-19 22:53:16 +0200

    Feat: add fucn to get pos_x ptr of image

--- sources/ft_game/ft_wall_texture.c
+++ sources/ft_game/ft_wall_texture.c
@@ -6,7 +6,7 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 22:08:13 by tlassere          #+#    #+#             */
-/*   Updated: 2024/04/19 22:36:36 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/04/19 22:52:25 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,12 @@ static unsigned int	ft_get_y_pos(long double ratio, float pos, unsigned int widt
 	return ((unsigned int)(ratio * pos) * width * sizeof(int));
 }
 
+static uint8_t	*ft_get_x_pos(mlx_texture_t *texture, float pos_x)
+{
+	return (texture->pixels + (unsigned int)(texture->width * pos_x)
+		* sizeof(int));
+}
+
 void	ft_print_line_texture(t_data *data, mlx_texture_t *texture,
 	t_data_wall wall, float pos_x)
 {
@@ -30,22 +36,25 @@ void	ft_print_line_texture(t_data *data, mlx_texture_t *texture,
 	int 			mid;
 	long double		ratio_height;
 	int				start_at;
-	int	step;
+	int				step;
 
 	step = 0;
 	start_at = 0;
 	mid	= 0;
-	if (texture && wall.height >= 0 && (unsigned int)wall.start < data->img.game->width && pos_x < 1)
+	if (texture && wall.height >= 0
+		&& (unsigned int)wall.start < data->img.game->width && pos_x < 1)
 	{
 		ratio_height = (long double)texture->height / (long double)wall.height;
 		if (wall.height > data->mlx->height)
 			start_at = (wall.height - data->mlx->height) / 2;
 		else
 			mid = (data->mlx->height - wall.height) / 2;
-		texture_x = texture->pixels + (unsigned int)(texture->width * pos_x) * sizeof(int);
+		texture_x = ft_get_x_pos(texture, pos_x);
 		while (step < data->mlx->height && step < wall.height)
 		{
-			mlx_put_pixel(data->img.game, wall.start, step + mid, ft_get_pixel_color(texture_x + ft_get_y_pos(ratio_height, step + start_at, texture->width)));
+			mlx_put_pixel(data->img.game, wall.start, step + mid,
+				ft_get_pixel_color(texture_x + ft_get_y_pos(ratio_height,
+				step + start_at, texture->width)));
 			step++;
 		}
 	}
